6792a9b764938ea3fcf59d3ba0fe05613ba4dc4c
fazza-abiyyu <fazza_abiyyu@qq.com>
2025-09-18 14:32:40 +0700

    Prepare for PyPI release

--- exnestai/__init__.py
+++ exnestai/__init__.py
@@ -2,3 +2,41 @@
 ExnestAI Python SDK
 A Python client for the ExnestAI API service
 """
+
+from .wrapper import ExnestAIWrapper
+from .client import ExnestAI
+from .models import (
+    ExnestMessage, 
+    ExnestResponse, 
+    ExnestStreamChunk, 
+    ExnestClientOptions, 
+    ExnestChatOptions,
+    ExnestModel,
+    ExnestModelProvider,
+    ExnestModelPricing,
+    ExnestModelLimits,
+    ExnestUsage,
+    ExnestChoice,
+    ExnestResponseData,
+    ExnestMeta
+)
+
+__version__ = "1.0.0"
+__author__ = "Fazza Abiyyu"
+__all__ = [
+    "ExnestAIWrapper",
+    "ExnestAI",
+    "ExnestMessage",
+    "ExnestResponse",
+    "ExnestStreamChunk",
+    "ExnestClientOptions",
+    "ExnestChatOptions",
+    "ExnestModel",
+    "ExnestModelProvider",
+    "ExnestModelPricing",
+    "ExnestModelLimits",
+    "ExnestUsage",
+    "ExnestChoice",
+    "ExnestResponseData",
+    "ExnestMeta"
+]
